344fb99a2b8c0258e5e1a161028a88f92b7bbcc8
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-22 17:14:59 +0300

    feat: add shared output field

---
+++ todo/src/components/text_outputs/my_task_desc/myTaskDesc.jsx
@@ -0,0 +1,12 @@
+import cl from './myTaskDesc.module.scss';
+
+const MyTaskDesc = ({desc}) => {
+
+  return (
+    <p className={cl.desc}>
+      {desc}
+    </p>
+  )
+};
+
+export default MyTaskDesc;
