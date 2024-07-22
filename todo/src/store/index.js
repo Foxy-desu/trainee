e34312fc27cb5631cb2e2bacd2e26f464a1bd9af
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-22 18:13:52 +0300

    feat: add middleware to hydrate store and function to push data from global store to local storage

--- todo/src/store/index.js
+++ todo/src/store/index.js
@@ -1,8 +1,32 @@
 import {configureStore} from '@reduxjs/toolkit';
 import taskReducer from './taskSlice.js';
 
+//this function sets all tasks from redux store to local storage,
+//and after that passes action forward (for other moddleware functions and reducers to use) 
+const tasksMiddleware = ({getState}) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem('tasks', JSON.stringify(getState()));
+    return result;
+  }
+};
+
+//this function is used to check local storage after the app starts and load data to redux store if there is any.
+// very useful to prevent data reset on page refreshes
+const getTasksFromLocalStorage =()=> {
+  if(localStorage.getItem('tasks') !== null && localStorage.getItem('tasks') !== 'undefined') {
+    return JSON.parse(localStorage.getItem('tasks'));
+  }
+};
+
 export const store = configureStore({
   reducer: {
     tasks: taskReducer,
   },
+  preloadedState: {
+    tasks: getTasksFromLocalStorage(),
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(tasksMiddleware)
+  }
 });
